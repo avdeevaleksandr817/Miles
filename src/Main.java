d3e71344c7876a818ed749e790b16e5c57061797
avdeevaleksandr817 <avdeevaleksandr817@gmail.com>
2022-02-15 19:07:37 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 800;
+        int bonus = 20;
+        int miles = price / bonus;
+        System.out.println("Бонусные мили:" + miles) ;
+    }
+}
